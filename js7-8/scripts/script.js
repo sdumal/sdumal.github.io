35caff9607f560ce1b8511381c23bdf82a7fb503
Stanislav <sdumal84@gmail.com>
2016-09-14 01:04:15 +0300

    Create script.js

---
+++ js7-8/scripts/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+(function() {
+    
+    $(document).ready(function() {
+
+        $(".tab:first").toggleClass("active--tab");
+        $("p span:first").show();
+
+        $(".tab").click(function() {
+            $(".active--tab").toggleClass("active--tab");
+            $("p span").hide();
+
+            $(this).toggleClass("active--tab");
+
+            var $tabAttr = $(this).html();
+
+            $('span[data = "'+$tabAttr+'"]').animate({opacity: "show"}, 1000);
+        })
+    })
+
+})();
